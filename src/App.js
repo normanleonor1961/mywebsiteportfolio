a047de883aee4cac88d7f6beac7bb446a71c5dc0
normanleonor1961 <109060058+normanleonor1961@users.noreply.github.com>
2022-09-11 10:15:08 +0800

    cleaned up files

--- src/App.js
+++ src/App.js
@@ -3,7 +3,7 @@ import "./App.css";
 function App() {
   return (
     <div className="App">
-      <h1>My Portfolio</h1>
+      <h1>My Portfolio is still underconstruction...</h1>
     </div>
   );
 }

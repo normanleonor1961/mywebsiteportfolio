df1daf8438ded7bf4172e6e1044ff837acf2c76f
normanleonor1961 <109060058+normanleonor1961@users.noreply.github.com>
2022-09-12 19:21:12 +0800

    added navbar

--- src/App.js
+++ src/App.js
@@ -1,9 +1,30 @@
 import "./App.css";
+import { Routes, Route } from "react-router-dom";
+import Home from "./containers/home";
+import About from "./containers/about";
+import Skills from "./containers/skills";
+import Resume from "./containers/resume";
+import Portfolio from "./containers/portfolio";
+import Contact from "./containers/contact";
+import Navbar from "./components/navBar";
 
 function App() {
   return (
     <div className="App">
-      <h1>My Portfolio is still underconstruction...</h1>
+      {/* particles */}
+      {/* navbar component */}
+      <Navbar />
+      {/* main page content */}
+
+      <Routes>
+        {/* create all routes */}
+        <Route path="/" index element={<Home />} />
+        <Route path="/about" index element={<About />} />
+        <Route path="/skills" index element={<Skills />} />
+        <Route path="/resume" index element={<Resume />} />
+        <Route path="/portfolio" index element={<Portfolio />} />
+        <Route path="/contact" index element={<Contact />} />
+      </Routes>
     </div>
   );
 }
